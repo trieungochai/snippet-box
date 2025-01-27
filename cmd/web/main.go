8f5d4b521c678b9184040d1dffcfc25521b111f6
trieungochai <trieungochai.dev@gmail.com>
2025-01-27 15:38:58 +0700

    refactor: extract handler functions into a separate file

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,70 +1,20 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 )
 
-// Define a home handler function which writes a byte slice containing
-// "Hello from Snippetbox" as the response body.
-func home(w http.ResponseWriter, r *http.Request) {
-	// Use the Header().Add() method to add a 'Server: Go' header to the response header map.
-	// The 1st parameter is the header name, and the 2nd parameter is the header value.
-	w.Header().Add("Server", "Go")
-
-	w.Write([]byte("Hello from Snippetbox"))
-}
-
-// Add a snippetView handler func
-func snippetView(w http.ResponseWriter, r *http.Request) {
-	// Extract the value of the id wildcard from the request using r.PathValue()
-	// and try to convert it to an integer using the strconv.Atoi() function.
-	// If it can't be converted to an integer, or the value is less than 1,
-	// we return a 404 page not found response.
-	id, err := strconv.Atoi(r.PathValue("id"))
-	if err != nil || id < 1 {
-		http.NotFound(w, r)
-		return
-	}
-
-	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
-}
-
-// Add a snippetCreate handler func
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a form for creating a new snippet..."))
-}
-
-// Add a snippetCreatePost handler function.
-func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-	// Use the w.WriteHeader() method to send a 201 status code.
-	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte("Save a new snippet..."))
-}
-
 func main() {
-	// Use the http.NewServeMux() function to initialize a new servemux,
-	// then register the home function as the handler for the "/" URL pattern.
 	mux := http.NewServeMux()
 
-	// Prefix the route patterns with the required HTTP method
-	// (for now, we will restrict all three routes to acting on GET requests).
-	mux.HandleFunc("GET /{$}", home) // Restrict this route to exact matches on / only
+	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
-	// Create the new route, which is restricted to POST requests only.
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-	// Print a log message to say that the server is starting.
 	log.Print("starting server on :4000")
 
-	// Use the http.ListenAndServe() function to start a new web server.
-	// We pass in 2 parameters: the TCP network address to listen on (in this case ":4000")
-	// and the servemux we just created. If http.ListenAndServe() returns an error
-	// we use the log.Fatal() function to log the error message and exit.
-	// Note that any error returned by http.ListenAndServe() is always non-nil.
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
