b851db58e26acff347768803b061ccce322b1153
trieungochai <trieungochai.dev@gmail.com>
2025-02-23 15:22:37 +0700

    [#5][#14] feat: update the SnippetModel.Insert() method so that it creates a new record in our snippets table and then returns the integer id for the new record

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -24,10 +24,29 @@ type SnippetModel struct {
 // Returns:
 // 1. The ID of the newly inserted snippet (integer).
 // 2. An error if something goes wrong.
-// The current implementation is a placeholder and doesn't actually interact with the db.
-// It always returns 0 and nil.
-func (sm *SnippetModel) Insert(title string, content string, expiresAt string) (int, error) {
-	return 0, nil
+func (sm *SnippetModel) Insert(title string, content string, expires_at string) (int, error) {
+	queryStmt := `INSERT INTO snippets (title, content, created, expires)
+    VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+
+	// Use the Exec() method on the embedded connection pool to execute the statement.
+	// The first parameter is the SQL statement,
+	// followed by the values for the placeholder parameters: title, content and expiry in that order.
+	// This method returns a sql.Result type, which contains some
+	// basic information about what happened when the statement was executed.
+	sqlResult, err := sm.DB.Exec(queryStmt, title, content, expires_at)
+	if err != nil {
+		return 0, err
+	}
+
+	// Use the LastInsertId() method on the result to get the ID of our
+	// newly inserted record in the snippets table.
+	id, err := sqlResult.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	// The ID returned has the type int64, so we convert it to an int type before returning.
+	return int(id), nil
 }
 
 // This will return a specific snippet based on its id.
