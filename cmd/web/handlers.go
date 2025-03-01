82e04fde2cf3eaae7e1728d2422aef7e3d9501ea
trieungochai <trieungochai.dev@gmail.com>
2025-03-02 02:04:58 +0700

    [#19][#20] feat: render a new view.tmpl template file

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"text/template"
 
 	// "text/template"
 
@@ -79,8 +80,27 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Write the snippet data as a plain-text HTTP response body.
-	fmt.Fprintf(w, "%+v", snippet)
+	// Initialize a slice containing the paths to the view.tmpl file,
+	// plus the base layout and navigation partial that we made earlier.
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/partials/nav.tmpl",
+		"./ui/html/pages/view.tmpl",
+	}
+
+	// Parse the template files...
+	tmpl, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	// And then execute them.
+	// Notice how we are passing in the snippet data (a models.Snippet struct) as the final parameter?
+	err = tmpl.ExecuteTemplate(w, "base", snippet)
+	if err != nil {
+		app.serverError(w, r, err)
+	}
 }
 
 // Change the signature of the home handler
