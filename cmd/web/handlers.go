4b976ed0a97067a24acf53cac2cc259aebe08247
trieungochai <trieungochai.dev@gmail.com>
2025-03-02 11:45:31 +0700

    [#19][#21] feat: templates actions and functions

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -7,8 +7,6 @@ import (
 	"strconv"
 	"text/template"
 
-	// "text/template"
-
 	"snippetbox.t10i.net/internal/models"
 )
 
@@ -23,41 +21,40 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	for _, snippet := range snippets {
-		fmt.Fprintf(w, "%+v\n", snippet)
-	}
-
 	// Initialize a slice containing the paths to the two files.
 	// It's important to note that
 	// the file containing our base template must be the *first* file in the slice.
-	// files := []string{
-	// 	"./ui/html/base.tmpl",
-	// 	// Include the navigation partial in the template files.
-	// 	"./ui/html/partials/nav.tmpl",
-	// 	"./ui/html/pages/home.tmpl",
-	// }
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/partials/nav.tmpl",
+		"./ui/html/pages/home.tmpl",
+	}
 
 	// Use the template.ParseFiles() func to read the files and store the templates in a template set.
 	// Notice that we use ... to pass the contents of the files slice as variadic arguments.
 	// If there's an error, we log the detailed error message,
 	// use the http.Error() function to send an Internal Server Error response to the user,
 	// and then return from the handler so no subsequent code is executed.
-	// tmpl, err := template.ParseFiles(files...)
-	// if err != nil {
-	// 	// Because the home handler is now a method against the application struct
-	// 	// it can access its fields, including the structured logger.
-	// 	// We'll use this to create a log entry at Error level containing the error message,
-	// 	// also including the request method and URI as attributes to assist with debugging.
-	// 	app.serverError(w, r, err) // Use the server serverError() helper
-	// 	return
-	// }
+	tmpl, err := template.ParseFiles(files...)
+	if err != nil {
+		// 	// Because the home handler is now a method against the application struct
+		// 	// it can access its fields, including the structured logger.
+		// 	// We'll use this to create a log entry at Error level containing the error message,
+		// 	// also including the request method and URI as attributes to assist with debugging.
+		app.serverError(w, r, err) // Use the server serverError() helper
+		return
+	}
 
-	// Use the ExecuteTemplate() method to write the content of the "base" template as the response body.
-	// err = tmpl.ExecuteTemplate(w, "base", nil)
-	// if err != nil {
-	// 	// And we also need to update the code here to use the structured logger too.
-	// 	app.serverError(w, r, err) // Use the serverError() helper
-	// }
+	// Create an instance of a templateData struct holding the slice of snippets.
+	data := templateData{
+		Snippets: snippets,
+	}
+
+	// Pass in the templateData struct when executing the template.
+	err = tmpl.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		app.serverError(w, r, err)
+	}
 }
 
 // Use the SnippetModel's Get() method to retrieve the data for a specific record based on its ID.
